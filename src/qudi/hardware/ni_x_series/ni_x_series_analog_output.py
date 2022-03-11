90d7142f5014920ea332fa723dde15700ceabdfb
LukePiewalker <48907973+LukePiewalker@users.noreply.github.com>
2022-03-11 11:43:54 +0100

    Quick rework of ni analog output module which is now capable of reading back the current output voltages from the ni card

--- src/qudi/hardware/ni_x_series/ni_x_series_analog_output.py
+++ src/qudi/hardware/ni_x_series/ni_x_series_analog_output.py
@@ -21,23 +21,14 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
-import ctypes
-from typing_extensions import final
-import warnings
-
-import numpy as np
 import nidaqmx as ni
-from nidaqmx._lib import lib_importer  # Due to NIDAQmx C-API bug needed to bypass property getter
-from nidaqmx.stream_readers import AnalogMultiChannelReader, CounterReader
-from nidaqmx.stream_writers import AnalogSingleChannelWriter
 
 from qudi.util.mutex import RecursiveMutex
 from qudi.core.configoption import ConfigOption
 from qudi.core.statusvariable import StatusVar
-from qudi.util.helpers import natural_sort
+from qudi.util.helpers import natural_sort, in_range
 
-from qudi.interface.process_control_interface import ProcessControlConstraints
-from qudi.interface.process_control_interface import ProcessSetpointInterface
+from qudi.interface.process_control_interface import ProcessControlConstraints, ProcessSetpointInterface
 
 
 class NIXSeriesAnalogOutput(ProcessSetpointInterface):
@@ -68,32 +59,31 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
                 keep_value: True         
     """
     _device_name = ConfigOption(name='device_name', default='Dev1', missing='warn')
-
+    # FIXME if dtype in config its interpreted as a string
     _setpoint_channels = ConfigOption(
         name='setpoint_channels',
         default={
-                'ao0': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
-                'ao1': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
-                'ao2': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
-                'ao3': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False}
-                },
+            'ao0': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
+            'ao1': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
+            'ao2': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False},
+            'ao3': {'unit': 'V', 'limits': (-10.0, 10.0), 'dtype': float, 'keep_value': False}
+        },
         missing='info'
     )
     _test = ConfigOption(name='test')
 
     _setpoints = StatusVar(name='current_setpoints', default=dict())
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._thread_lock = RecursiveMutex()
 
-        self._system = None
         self._device_handle = None
         self._ao_ch_names = list()
         self._ao_task_handles = dict()
-        self._ao_writer_handles = dict()
+        self._ai_task = None
         self._keep_values = None
-        
+
         self._is_active = False
         self.__constraints = None
 
@@ -101,7 +91,6 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         """
             Starts up the NI-card and performs sanity checks.
         """
-        # TODO Need to check the value range of config that it does not exceed ni limits
         # Check if device is connected and set device to use
         device_names = ni.system.System().devices.device_names
         if self._device_name.lower() not in set(dev.lower() for dev in device_names):
@@ -116,16 +105,16 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         self._device_handle = ni.system.Device(self._device_name)
 
         # Check if given ao channel names from the cfg are a subset of the available ao channel names
-        self.__avlbl_ao_ch_names = tuple(
+        avlbl_ao_ch_names = tuple(
             term.rsplit('/', 1)[-1].lower() for term in self._device_handle.ao_physical_chans.channel_names)
         ao_ch_names_set = set(self._extract_ch_name(ao_ch_name) for ao_ch_name in self._setpoint_channels)
-        
-        invalid_sources = ao_ch_names_set.difference(set(self.__avlbl_ao_ch_names))
+
+        invalid_sources = ao_ch_names_set.difference(set(avlbl_ao_ch_names))
         if invalid_sources:
             self.log.error('Invalid analog source channels encountered. Following sources will '
-                            'be ignored:\n  {0}\nValid analog input channels are:\n  {1}'
-                            ''.format(', '.join(natural_sort(invalid_sources)),
-                                        ', '.join(self.__avlbl_ao_ch_names)))
+                           'be ignored:\n  {0}\nValid analog input channels are:\n  {1}'
+                           ''.format(', '.join(natural_sort(invalid_sources)),
+                                     ', '.join(avlbl_ao_ch_names)))
         self._ao_ch_names = natural_sort(ao_ch_names_set.difference(invalid_sources))
 
         if not self._ao_ch_names:
@@ -133,12 +122,18 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
                 'No valid analog ouput sources defined in config. Activation of '
                 'NIXSeriesAnalogOutput failed!'
             )
-        
+
         # Initialization of hardware constraints defined in the config file
         units = {ch: d['unit'] for ch, d in self._setpoint_channels.items() if 'unit' in d}
         limits = {ch: d['limits'] for ch, d in self._setpoint_channels.items() if 'limits' in d}
         dtypes = {ch: d['dtype'] for ch, d in self._setpoint_channels.items() if 'dtype' in d}
         self._keep_values = {ch: d['keep_value'] for ch, d in self._setpoint_channels.items() if 'keep_value' in d}
+
+        # Check that specified limits do not exceed Ni HArdware limits
+        for ao_chan in self._ao_ch_names:
+            assert all(in_range(val, *self._device_handle.ao_voltage_rngs)[0]
+                       for val in limits[ao_chan]), \
+                f'Specified limits {limits[ao_chan]} exceed the hardware limits {self._device_handle.ao_voltage_rngs}'
 
         self.__constraints = ProcessControlConstraints(
             setpoint_channels=tuple(self._setpoint_channels),
@@ -146,16 +141,12 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
             limits=limits,
             dtypes=dtypes
         )
-        
-        if not self._setpoints:
-            self._setpoints = {ch: min(max(lim[0], 0), lim[1]) for ch, lim in
-                            self.__constraints.channel_limits.items() if
-                            ch in self._setpoint_channels}
 
     def on_deactivate(self):
         if self._is_active:
             for ao_ch_name in self._ao_ch_names:
                 self.terminate_ao_task(ao_ch_name)
+            self.terminate_ai_task()
 
     @property
     def constraints(self):
@@ -191,8 +182,11 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
 
         @return dict: Currently set target values (values) for all channels (keys)
         """
+        assert self._is_active, 'Please activate first!'
+
         with self._thread_lock:
-            return self._setpoints.copy()
+            voltages = {ch: val for ch, val in zip(self._ao_ch_names, self.ai_task.read())}
+            return voltages
 
     @setpoints.setter
     def setpoints(self, values):
@@ -202,11 +196,11 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         """
         assert self._is_active, 'Please activate first!'
 
-        assert set(values).issubset(self._setpoints), \
-            f'Invalid setpoint channels encountered. Valid channels are: {set(self._setpoints)}'
+        assert set(values).issubset(self._setpoint_channels), \
+            f'Invalid setpoint channels encountered. Valid channels are: {set(self._setpoint_channels)}'
         assert all(self.__constraints.channel_value_in_range(v, ch)[0] for ch, v in
                    values.items()), 'One or more setpoints out of allowed value bounds'
-        
+
         with self._thread_lock:
             for ch, v in values.items():
                 self.set_setpoint(ch, v)
@@ -220,20 +214,28 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         with self._thread_lock:
             if active != self._is_active:
                 if active and self.module_state() != 'locked':
+                    self._ai_task = ni.Task()
                     for ao_ch_name in self._ao_ch_names:
                         ao_task = ni.Task(ao_ch_name)
                         ao_phys_ch = f"/{self._device_name}/{ao_ch_name}"
                         ao_task.ao_channels.add_ao_voltage_chan(
-                                                                physical_channel=ao_phys_ch,
-                                                                min_val=self.__constraints.channel_limits[ao_ch_name][0], 
-                                                                max_val=self.__constraints.channel_limits[ao_ch_name][1]
-                                                                )
+                            physical_channel=ao_phys_ch,
+                            min_val=self.__constraints.channel_limits[ao_ch_name][0],
+                            max_val=self.__constraints.channel_limits[ao_ch_name][1]
+                        )
                         self._ao_task_handles[ao_ch_name] = ao_task
+
+                        self.ai_task.ai_channels.add_ai_voltage_chan(
+                            f'/{self._device_name}/_{ao_ch_name}_vs_aognd',
+                            min_val=self.__constraints.channel_limits[ao_ch_name][0],
+                            max_val=self.__constraints.channel_limits[ao_ch_name][1]
+                        )
 
                     self.module_state.lock()
                 elif not active and self.module_state() == 'locked':
                     for ao_ch_name in self._ao_ch_names:
                         self.terminate_ao_task(ao_ch_name)
+                    self.terminate_ai_task()
                     self.module_state.unlock()
                 self._is_active = active
 
@@ -243,17 +245,16 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         @param str channel: Channel to set
         @param float|int value: Setpoint value to set
         """
-        
+
         assert self._is_active, 'Please activate first!'
-        
-        assert channel in self._setpoints, \
+
+        assert channel in self._setpoint_channels, \
             f'Invalid setpoint channel "{channel}" encountered. Valid channels are: ' \
-            f'{set(self._setpoints)}'
+            f'{set(self._setpoint_channels)}'
         assert self.__constraints.channel_value_in_range(value, channel)[0], \
             'Setpoint out of allowed value bounds'
         with self._thread_lock:
-            self._setpoints[channel] = self.__constraints.channel_dtypes[channel](value)
-            self._ao_task_handles[channel].write(self._setpoints[channel])
+            self._ao_task_handles[channel].write(self.__constraints.channel_dtypes[channel](value))
 
     def get_setpoint(self, channel):
         """ Get current setpoint for a single channel.
@@ -261,17 +262,20 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
         @param str channel: Channel to get the setpoint for
         @return float|int: The current setpoint for <channel>
         """
-        assert channel in self._setpoints, f'Invalid setpoint channel "{channel}" encountered.' \
-                                           f' Valid channels are: {set(self._setpoints)}'
+        assert self._is_active, 'Please activate first!'
+
+        assert channel in self._ao_ch_names, f'Invalid setpoint channel "{channel}" encountered.' \
+                                             f' Valid channels are: {set(self._ao_ch_names)}'
+
         with self._thread_lock:
-            return self._setpoints[channel]
+            return self.setpoints[channel]
 
     def terminate_ao_task(self, ao_ch_name):
         """
             Reset analog output to 0 if keep_value flag is False
         """
         if not self._keep_values[ao_ch_name]:
-            self.set_setpoint(ao_ch_name, 0)    
+            self.set_setpoint(ao_ch_name, 0)
 
         try:
             if not self._ao_task_handles[ao_ch_name].is_task_done():
@@ -282,7 +286,18 @@ class NIXSeriesAnalogOutput(ProcessSetpointInterface):
             err = -1
         finally:
             del self._ao_task_handles[ao_ch_name]
-    
+
+    def terminate_ai_task(self):
+        try:
+            if not self.ai_task.is_task_done():
+                self.ai_task.stop()
+            self.ai_task.close()
+        except ni.DaqError:
+            self.log.exception('Error while trying to terminate analog output task.')
+            err = -1
+        finally:
+            del self.ai_task
+
     @staticmethod
     def _extract_ch_name(ch_str):
         """
