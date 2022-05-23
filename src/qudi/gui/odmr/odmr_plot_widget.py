2693635135285e334280efaf1001eded03464514
Neverhorst <tomek.qudi@gmail.com>
2022-05-23 11:23:05 +0200

    Refactored ODMR GUI to new plot widgets

--- src/qudi/gui/odmr/odmr_plot_widget.py
+++ src/qudi/gui/odmr/odmr_plot_widget.py
@@ -25,8 +25,7 @@ __all__ = ('OdmrPlotWidget',)
 import pyqtgraph as pg
 from PySide2 import QtCore, QtWidgets
 
-from qudi.util.widgets.scan_2d_widget import ScanImageItem
-from qudi.util.widgets.colorbar import ColorBarWidget, ColorBarMode
+from qudi.util.widgets.plotting.image_widget import ImageWidget
 from qudi.util.colordefs import QudiPalettePale as palette
 
 
@@ -37,8 +36,7 @@ class OdmrPlotWidget(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        main_layout = QtWidgets.QGridLayout()
-        main_layout.setColumnStretch(0, 1)
+        main_layout = QtWidgets.QVBoxLayout()
         main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(main_layout)
 
@@ -61,39 +59,26 @@ class OdmrPlotWidget(QtWidgets.QWidget):
         self._plot_widget.setLabel('bottom', text='Frequency', units='Hz')
         self._plot_widget.setLabel('left', text='Signal')
         self._plot_widget.showGrid(x=True, y=True)
-        main_layout.addWidget(self._plot_widget, 0, 0)
+        main_layout.addWidget(self._plot_widget)
 
         # Create matrix plot
-        self._image_widget = pg.PlotWidget()
-        self._image_widget.getPlotItem().setContentsMargins(0, 1, 5, 2)
-        self._image_item = ScanImageItem()
-        self._image_widget.addItem(self._image_item)
+        self._image_widget = ImageWidget()
+        # self._image_widget.getPlotItem().setContentsMargins(0, 1, 5, 2)
+        self._image_item = self._image_widget.image_item
         self._image_widget.setMinimumWidth(100)
         self._image_widget.setMinimumHeight(100)
         self._image_widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
                                          QtWidgets.QSizePolicy.Expanding)
         self._image_widget.setFocusPolicy(QtCore.Qt.FocusPolicy.NoFocus)
         # self._image_widget.setAspectLocked(lock=True, ratio=1.0)
-        self._image_widget.setLabel('bottom', text='Frequency', units='Hz')
-        self._image_widget.setLabel('left', text='Scan Line')
-        main_layout.addWidget(self._image_widget, 1, 0)
-
-        # Create colorbar
-        self._colorbar = ColorBarWidget()
-        self._colorbar.set_label(text='Signal', unit='P')
-        self._colorbar.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
-        if self._colorbar.mode is ColorBarMode.PERCENTILE:
-            self._image_item.percentiles = self._colorbar.percentiles
-        else:
-            self._image_item.percentiles = None
-        self._colorbar.sigModeChanged.connect(self._colorbar_mode_changed)
-        self._colorbar.sigLimitsChanged.connect(self._colorbar_limits_changed)
-        self._colorbar.sigPercentilesChanged.connect(self._colorbar_percentiles_changed)
-        main_layout.addWidget(self._colorbar, 0, 1, 2, 1)
+        self._image_widget.set_axis_label('bottom', label='Frequency', unit='Hz')
+        self._image_widget.set_axis_label('left', label='Scan Line')
+        self._image_widget.set_data_label(label='Signal', unit='P')
+        main_layout.addWidget(self._image_widget)
 
     def set_signal_label(self, channel, unit):
         self._plot_widget.setLabel('left', text=channel, units=unit)
-        self._colorbar.set_label(text=channel, unit=unit)
+        self._image_widget.set_data_label(label=channel, unit=unit)
 
     def set_data(self, frequency, image=None, signal=None, fit=None):
         if image is not None:
@@ -104,22 +89,11 @@ class OdmrPlotWidget(QtWidgets.QWidget):
             self.set_fit_data(frequency, fit)
 
     def set_image_data(self, frequency, data):
-        if data is None:
-            self._image_item.clear()
+        if frequency is None or data is None:
+            extent = None
         else:
-            if self._colorbar.mode is ColorBarMode.PERCENTILE:
-                self._image_item.set_image(image=data, autoLevels=False)
-                levels = self._image_item.levels
-                if levels is not None:
-                    self._colorbar.set_limits(*levels)
-            else:
-                self._image_item.set_image(image=data,
-                                           autoLevels=False,
-                                           levels=self._colorbar.limits)
-            if frequency is not None:
-                self._image_item.set_image_extent(
-                    ((frequency[0], frequency[-1]), (0, data.shape[1]))
-                )
+            extent = ((frequency[0], frequency[-1]), (0, data.shape[1]))
+        self._image_widget.set_image(data, extent)
 
     def set_signal_data(self, frequency, data):
         if data is None:
@@ -132,22 +106,3 @@ class OdmrPlotWidget(QtWidgets.QWidget):
             self._fit_data_item.clear()
         else:
             self._fit_data_item.setData(y=data, x=frequency)
-
-    @QtCore.Slot(object)
-    def _colorbar_mode_changed(self, mode):
-        if mode is ColorBarMode.PERCENTILE:
-            self._colorbar_percentiles_changed(self._colorbar.percentiles)
-        else:
-            self._colorbar_limits_changed(self._colorbar.limits)
-
-    @QtCore.Slot(tuple)
-    def _colorbar_limits_changed(self, limits):
-        self._image_item.percentiles = None
-        self._image_item.setLevels(limits)
-
-    @QtCore.Slot(tuple)
-    def _colorbar_percentiles_changed(self, percentiles):
-        self._image_item.percentiles = percentiles
-        levels = self._image_item.levels
-        if levels is not None:
-            self._colorbar.set_limits(*levels)
