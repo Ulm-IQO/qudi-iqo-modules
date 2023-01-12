8a1627796178de25b6668e5969ad5fb500980ef1
takuya <takuya.kitamura@uni-ulm.de>
2023-01-12 14:25:23 +0100

    draft of Voltcraft_DSP3005V.py

--- src/qudi/hardware/power_supply/Voltcraft_DSP3005V.py
+++ src/qudi/hardware/power_supply/Voltcraft_DSP3005V.py
@@ -21,35 +21,84 @@ If not, see <https://www.gnu.org/licenses/>.
 import pyvisa
 from qudi.core.configoption import ConfigOption
 from qudi.interface.process_control_interface import ProcessControlInterface
+from typing import Iterable, Mapping, Union, Optional, Tuple, Type, Dict
 
+_Real = Union[int, float]
 
 
 
 class Voltcraft3005V(ProcessControlInterface):
 
-    _visa_address = ConfigOption('visa_address', missing='error')
+#    _visa_address = ConfigOption('visa_address', missing='error')
 
     def on_activate(self):
+        pass
 
-        self.rm = pyvisa.ResourceManager()
-        self.inst = self.rm.open_resource(self._visa_address,
-                                          write_termination='\r\n',
-                                          read_termination='\r\n',
-                                          baud_rate=115200)
-
+        # self.rm = pyvisa.ResourceManager()
+        # self.inst = self.rm.open_resource(self._visa_address,
+        #                                   write_termination='\r\n',
+        #                                   read_termination='\r\n',
+        #                                   baud_rate=115200)
+        #
 
     def on_deactivate(self):
+        pass
 
 
     def constraints(self):
+        pass
+    @property
+    def is_active(self) -> bool:
+        """ Current activity state.
+        State is bool type and refers to active (True) and inactive (False).
+        """
+        pass
 
-    def is_active(self):
+    @is_active.setter
+    def is_active(self, active: bool):
+        """ Set activity state.
+        State is bool type and refers to active (True) and inactive (False).
+        """
+        pass
 
-    def set_control_value(self, value):
+    @property
+    def setpoints(self) -> Dict[str, _Real]:
+        """ The current setpoints (values) for all channels (keys) """
+        pass
 
-    def get_control_value(self):
+    @setpoints.setter
+    def setpoints(self, values: Mapping[str, _Real]):
+        """ Set the setpoints (values) for all channels (keys) at once """
+        pass
 
-    def get_control_unit(self):
+    def set_activity_state(self, active: bool) -> None:
+        """ Set activity state. State is bool type and refers to active (True) and inactive (False).
+        """
+        pass
+
+    def set_setpoint(self, value: _Real, channel: str) -> None:
+        """ Set new setpoint for a single channel """
+        pass
+
+    def get_setpoint(self, channel: str) -> _Real:
+        """ Get current setpoint for a single channel """
+        pass
+
+    @property
+    def process_values(self) -> Dict[str, _Real]:
+        """ Read-Only property returning a snapshot of current process values (values) for all
+        channels (keys).
+        """
+        pass
+
+    def set_activity_state(self, active: bool) -> None:
+        """ Set activity state. State is bool type and refers to active (True) and inactive (False).
+        """
+        pass
+
+    def get_process_value(self, channel: str) -> _Real:
+        """ Get current process value for a single channel """
+        pass
 
 
 
