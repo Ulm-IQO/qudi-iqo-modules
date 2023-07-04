25543009f56b68da3867c2db67f07c6335477ecc
Roberto Sailer <roberto.sailer@uni-ulm.de>
2023-07-04 14:07:47 +0200

    Inserted the changes from Timo's branch "tilt_correction_main"

--- src/qudi/interface/scanning_probe_interface.py
+++ src/qudi/interface/scanning_probe_interface.py
@@ -250,6 +250,7 @@ class ScanData:
         self._data = None
         self._position_data = None
         self._target_at_start = target_at_start
+        self._tilt_correction_info = {'enabled': False}
         # TODO: Automatic interpolation onto rectangular grid needs to be implemented (for position feedback HW)
         return
 
@@ -423,6 +424,14 @@ class ScanData:
         if dict_repr['timestamp'] is not None:
             new_inst._timestamp = datetime.datetime.fromtimestamp(dict_repr['timestamp'])
         return new_inst
+
+    @property
+    def tilt_correction_info(self):
+        return self._tilt_correction_info
+
+    @tilt_correction_info.setter
+    def tilt_correction_info(self, info_dict):
+        self._tilt_correction_info = info_dict
 
 
 class ScannerChannel:
@@ -665,13 +674,28 @@ class CoordinateTransformMixin:
         self._coordinate_transform = transform_func
 
     def move_absolute(self, position, velocity=None, blocking=False):
-        return super().move_absolute(self.coordinate_transform(position), velocity, blocking)
+        new_pos_bare = super().move_absolute(self.coordinate_transform(position), velocity, blocking)
+        return self.coordinate_transform(new_pos_bare, inverse=True)
 
     def move_relative(self, distance, velocity=None, blocking=False):
-        return super().move_relative(self.coordinate_transform(distance), velocity, blocking)
+        new_pos_bare =  super().move_relative(self.coordinate_transform(distance), velocity, blocking)
+        return self.coordinate_transform(new_pos_bare, inverse=True)
 
     def get_target(self):
         return self.coordinate_transform(super().get_target(), inverse=True)
 
     def get_position(self):
         return self.coordinate_transform(super().get_position(), inverse=True)
+
+    def get_scan_data(self):
+
+        scan_data = super().get_scan_data()
+        if scan_data:
+            # todo: transform_func not needed, replace with valuable info (matrix, angle, ..)
+            tilt_info = {'enabled': self.coordinate_transform_enabled,
+                         'transform_func': self._coordinate_transform}
+            scan_data.tilt_correction_info = tilt_info
+
+            self.log.debug(f"Get scan data for titl corrected hw called. Info: {tilt_info}")
+
+        return scan_data
