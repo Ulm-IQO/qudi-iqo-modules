2087117399e4b1c59fd3cd9a73db5720186f1c2b
qku <kilian.unterguggenberger@hu-berlin.de>
2024-05-09 09:38:48 +0200

    update documentation

--- src/qudi/hardware/wavemeter/high_finesse_pid.py
+++ src/qudi/hardware/wavemeter/high_finesse_pid.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+
+"""
+This file contains the qudi hardware module for the (optional) laser control functionality of a HighFinesse wavemeter.
+
+Copyright (c) 2021, the qudi developers. See the AUTHORS.md file at the top-level directory of this
+distribution and on <https://github.com/Ulm-IQO/qudi-iqo-modules/>
+
+This file is part of qudi.
+
+Qudi is free software: you can redistribute it and/or modify it under the terms of
+the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with qudi.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from typing import Tuple, Dict
 
 from scipy.constants import lambda2nu
@@ -13,7 +34,7 @@ import qudi.hardware.wavemeter.high_finesse_constants as high_finesse_constants
 
 class HighFinessePID(PIDControllerInterface):
     """
-    HighFinessePID class
+    Laser control functionality of a HighFinesse wavemeter.
 
     Example config for copy-paste:
 
