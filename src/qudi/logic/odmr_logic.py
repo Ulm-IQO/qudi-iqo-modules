9743e56d23da1ca576b485fa9da47b58775fa7cf
simon1schmitt <simon.schmitt@diatope.com>
2024-06-17 08:21:07 +0200

    Revert "remove old fits from odmr when starting a new measurement"
    This reverts commit 3acdf2db0d67076b5ae466ed64b7c845ccc1f64d.

--- src/qudi/logic/odmr_logic.py
+++ src/qudi/logic/odmr_logic.py
@@ -546,10 +546,7 @@ class OdmrLogic(LogicBase):
                 self.sigScanStateUpdated.emit(False)
                 return
 
-            for channel, range_data in self._raw_data.items():
-                for range_index, _ in enumerate(range_data):
-                    self._fit_results[channel][range_index] = None
-                    self.sigFitUpdated.emit(self._fit_results[channel][range_index], channel, range_index)
+            # ToDo: Clear old fit
             self._elapsed_sweeps = 0
             self._elapsed_time = 0.0
             self.sigElapsedUpdated.emit(self._elapsed_time, self._elapsed_sweeps)
