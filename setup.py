b1500f286c698154fda0f6bd53642c7fced65cba
Tobias Spohn <kai.spohn@uni-ulm.de>
2025-01-31 19:58:09 +0100

    fix qudi-core version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_namespace_packages
 
 
 unix_dep = [
-    'qudi-core==1.5.2',
+    'qudi-core==1.5.1',
     'entrypoints==0.4',
     'fysom==2.1.6',
     'lmfit==1.3.2',
@@ -21,7 +21,7 @@ unix_dep = [
 ]
 
 windows_dep = [
-    'qudi-core==1.5.2',
+    'qudi-core==1.5.1',
     'entrypoints==0.4',
     'fysom==2.1.6',
     'lmfit==1.3.2',
