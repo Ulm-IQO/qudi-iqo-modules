f967429a07095be6094a33284fb6a6a6d13da9c7
Geegee <wgolff@googlemail.com>
2025-04-10 10:35:10 +0200

    Adding more hardware constraints. Making them more transparent by moving the hardware specific values to the top level of the class.
    Handling the state of the magnet (is it moving?) to reasonably
    communicate with logic and gui (no commands will be send while
    the stage is still moving).

--- src/qudi/hardware/motor/motor_kanfit_movement_ulm.py
+++ src/qudi/hardware/motor/motor_kanfit_movement_ulm.py
@@ -21,6 +21,7 @@ If not, see <https://www.gnu.org/licenses/>.
 """
 from collections import OrderedDict
 from typing import Dict, Any
+import time
 
 import serial
 import numpy as np
@@ -28,8 +29,25 @@ import numpy as np
 from qudi.interface.motor_interface import MotorInterface
 from qudi.core.configoption import ConfigOption, MissingOption
 
+def moving_p(finish_time):
+    if time.time() < finish_time:
+        return True
+    else:
+        return False
+
 class KanfitMotorStage(MotorInterface):
     """ Control class for the 3 axes magnet motor stage in Ulm university."""
+    # hardware specifics
+    _thread_pitch_xy = 3e-3
+    # for z this is not exactly the thread pitch, but one
+    # also has to consider the gear ratio.
+    _thread_pitch_z = 1.16e-3
+    # determined through experiment.
+    _vel_x = 0.003467
+    _vel_y = 0.003467
+    _vel_z = 0.003467 / 3
+    # number of positions between min and max value (arb. chosen)
+    _steps = 1000
 
     _com_port = ConfigOption('com_port', 'COM8', missing='warn')
     _baud_rate = ConfigOption('baud_rate', 115_200, missing='warn')
@@ -49,8 +67,15 @@ class KanfitMotorStage(MotorInterface):
     # we can only send absolute positions to the stage.
     # relative movement therefore has to be emulated by comparing
     # to the previous position
-    _abs_pos = (0.0, 0.0, 0.0)
+
     _pos = dict()
+    _target_pos = dict()
+    _mov = False
+    _mov_time = 0.0
+    _start_time = 0.0
+    _estimated_time = 0.0
+    _finish_time = 0.0
+
 
     def on_activate(self):
         """ Initialisation performed during activation of the module.
@@ -61,6 +86,11 @@ class KanfitMotorStage(MotorInterface):
         self._pos[self._first_axis_label] = 0.0
         self._pos[self._second_axis_label] = 0.0
         self._pos[self._third_axis_label] = 0.0
+
+        self._vels = dict()
+        self._vels[self._first_axis_label] = self._vel_x
+        self._vels[self._second_axis_label] = self._vel_y
+        self._vels[self._third_axis_label] = self._vel_z
 
         if self.con.readable():
             self.log.info("Successfully established connection to the stage.")
@@ -86,7 +116,18 @@ class KanfitMotorStage(MotorInterface):
 
         @return dict: with the axis label as key and the axis status as value.
         """
-        pass
+        rtrn = {}
+        self._mov = moving_p(self._finish_time)
+
+        if param_list:
+            for key in param_list:
+                rtrn[key] = not self._mov
+        else:
+            rtrn[self._first_axis_label] = not self._mov
+            rtrn[self._second_axis_label] = not self._mov
+            rtrn[self._third_axis_label] = not self._mov
+
+        return rtrn
 
 
     def get_constraints(self):
@@ -113,10 +154,10 @@ class KanfitMotorStage(MotorInterface):
                  'ramp': None,
                  'pos_min': self._min_first,
                  'pos_max': self._max_first,
-                 'pos_step': (self._max_first - self._min_first) / 4096,
-                 'pos_accuracy': (self._max_first - self._min_first) / 4096,
-                 'vel_min': None,
-                 'vel_max': None,
+                 'pos_step': (self._max_first - self._min_first) / self._steps,
+                 'pos_accuracy': (self._max_first - self._min_first) / self._steps,
+                 'vel_min': self._vels[self._first_axis_label],
+                 'vel_max': self._vels[self._first_axis_label],
                  'vel_step': None,
                  'acc_min': None,
                  'acc_max': None,
@@ -128,10 +169,10 @@ class KanfitMotorStage(MotorInterface):
                  'ramp': None,
                  'pos_min': self._min_second,
                  'pos_max': self._max_second,
-                 'pos_step': (self._max_second - self._min_second) / 4096,
-                 'pos_accuracy': (self._max_second - self._min_second) / 4096,
-                 'vel_min': None,
-                 'vel_max': None,
+                 'pos_step': (self._max_second - self._min_second) / self._steps,
+                 'pos_accuracy': (self._max_second - self._min_second) / self._steps,
+                 'vel_min': self._vels[self._second_axis_label],
+                 'vel_max': self._vels[self._second_axis_label],
                  'vel_step': None,
                  'acc_min': None,
                  'acc_max': None,
@@ -142,10 +183,10 @@ class KanfitMotorStage(MotorInterface):
                  'unit': 'm', 'ramp': None,
                  'pos_min': self._min_third,
                  'pos_max': self._max_third,
-                 'pos_step': (self._max_third - self._min_third) / 4096,
-                 'pos_accuracy': (self._max_third - self._min_third) / 4096,
-                 'vel_min': None,
-                 'vel_max': None,
+                 'pos_step': (self._max_third - self._min_third) / self._steps,
+                 'pos_accuracy': (self._max_third - self._min_third) / self._steps,
+                 'vel_min': self._vels[self._third_axis_label],
+                 'vel_max': self._vels[self._third_axis_label],
                  'vel_step': None,
                  'acc_min': None,
                  'acc_max': None,
@@ -177,22 +218,31 @@ class KanfitMotorStage(MotorInterface):
         # TODO implement safety checks
         self.log.warning(f"what is the input to move_abs {param_dict}")
 
+        def calc_total_movement_duration(old_param_dict, new_param_dict):
+            """Given a set of movements calculate an estimate for how long it will take
+            to complete the movement."""
+            duration = 0.0
+            # TODO axes selection shouldn't be config dependent
+            for key in old_param_dict:
+                dist = np.abs(new_param_dict[key] - old_param_dict[key])
+                duration += dist / self._vels[key]
+            return duration
+
         def movement_to_num(mov, ax_id):
             """Translates the movement in meter to a number, that is understood
             by the stage."""
-            thread_pitch_xy = 3e-3
-            thread_pitch_z = 1.16e-3
+
             # one rotation corresponds to 2 ** 12
             bit_encoding = 12
             enc = mov * 2 ** bit_encoding
             if ax_id == 'z':
-                rot = hex(int(enc / thread_pitch_z))
+                rot = hex(int(enc / self._thread_pitch_z))
             else:
-                rot = hex(int(enc / thread_pitch_xy))
+                rot = hex(int(enc / self._thread_pitch_xy))
             return rot
 
         def format_hex(hn, ln):
-            """ Format number into hex that is understood by the stage"""
+            """ Format number into hex, needed for the stage"""
             _, rhn = hn.split('x')
             if len(rhn) < ln:
                 thn = ""
@@ -211,12 +261,24 @@ class KanfitMotorStage(MotorInterface):
                 ffhn += hh + tt + " "
             return ffhn
 
+        # first check if we are still moving
+        self._mov = moving_p(self._finish_time)
+        # if we are already moving just return
+        # should this throw an error?
+        if self._mov:
+            self.log.warning("Got a movement command although moving already.")
+            return 0
+
+        # calculate how long we will need for all the movement.
+        self._start_time = time.time()
+        self._mov_time = calc_total_movement_duration(param_dict, self._pos)
+        self._finish_time = self._mov_time + self._start_time
         # the base command contain the 3 bytes that are in front of
         # every movement command.
         base_command = "23 63 3E B3"
         hns = list()
         self.log.warning(f"self._pos {self._pos} before update")
-        self._pos.update(param_dict)
+        self._target_pos.update(param_dict)
         self.log.warning(f"self._pos {self._pos} after update")
         for key in self._pos:
             rot = movement_to_num(self._pos[key],  key)
@@ -252,7 +314,72 @@ class KanfitMotorStage(MotorInterface):
         @return dict: with keys being the axis labels and value the current
                       position.
         """
-        return self._pos
+        # we will need to calculate some estimate where we are given the time we already
+        # moved and the speed that we have
+
+        # the way the stage moves is ordered according to the axes.
+        # meaning 1. axis, 2. axis then 3rd axis.
+        rtrn = dict()
+
+        # are we still moving?
+        if self._finish_time < time.time():
+            self._mov = True
+        else:
+            self._mov = False
+
+        # begin by calculating the current position
+        if self._mov:
+            tmp_d0 = {}
+            time_running = time.time() - self._start_time
+
+            dist_x = np.abs(self._target_pos[self._first_axis_label] - self._pos[self._first_axis_label])
+            dist_y = np.abs(self._target_pos[self._second_axis_label] - self._pos[self._second_axis_label])
+
+            time_mov_x = dist_x / self._vel_x
+            time_mov_y = dist_y / self._vel_y
+
+            # if we are moving we can only be sure that
+            # there was a movement in the first axis movement
+            # we move one axis at a time.
+            if time_running < time_mov_x:
+                dx = time_running * self._vel_x
+                cur_x = dx + self._pos[self._first_axis_label]
+
+                tmp_d0[self._first_axis_label] = cur_x
+                tmp_d0[self._second_axis_label] = self._pos[self._second_axis_label]
+                tmp_d0[self._third_axis_label] = self._pos[self._third_axis_label]
+            elif time_running < (time_mov_x + time_mov_y):
+                dy = (time_running - time_mov_x) * self._vel_y
+                cur_y = dy + self._pos[self._second_axis_label]
+
+                tmp_d0[self._first_axis_label] = self._target_pos[self._first_axis_label]
+                tmp_d0[self._second_axis_label] = cur_y
+                tmp_d0[self._third_axis_label] = self._pos[self._third_axis_label]
+            else:
+                dz = (time_running - time_mov_x - time_mov_y) * self._vel_z
+                cur_z = dz + self._pos[self._third_axis_label]
+
+                tmp_d0[self._first_axis_label] = self._target_pos[self._first_axis_label]
+                tmp_d0[self._second_axis_label] = self._target_pos[self._second_axis_label]
+                tmp_d0[self._third_axis_label] = cur_z
+            if param_list:
+                for key in param_list:
+                    rtrn[key] = self._tmp_d0[key]
+            else:
+                for key in self._pos:
+                    rtrn[key] = self._tmp_d0[key]
+        # if there is no movement we can just assume that
+        # the last position we set was reached.
+        else:
+            if param_list:
+                for key in param_list:
+                    rtrn[key] = self._pos[key]
+            else:
+                for key in self._pos:
+                    rtrn[key] = self._pos[key]
+
+        # at last we return what is queried by the function
+        return rtrn
 
 
     def calibrate(self, param_list=None):
@@ -271,7 +398,13 @@ class KanfitMotorStage(MotorInterface):
         different for each stage.
         """
         # this command sets the zero of the stage
-        self.con.write(bytes.fromhex('23 63 3e 77'))
+        # we set the encoders to 0
+        self.con.write(bytes.fromhex('23 63 3e C0'))
+        time.sleep(0.1)
+        self.con.write(bytes.fromhex('23 63 3e C1'))
+        time.sleep(0.1)
+        self.con.write(bytes.fromhex('23 63 3e C2'))
+        time.sleep(0.1)
         return
 
     def get_velocity(self, param_list=None):
@@ -285,7 +418,15 @@ class KanfitMotorStage(MotorInterface):
 
         @return dict : with the axis label as key and the velocity as item.
         """
-        pass
+        rtrn = {}
+        if param_list:
+            for key in param_list:
+                rtrn[param] =  self._vels[key]
+        else:
+            for key in self._vels:
+                rtrn[key] = self._vels[key]
+
+        return
 
     def set_velocity(self, param_dict):
         """ Write new value for velocity.
@@ -305,4 +446,5 @@ class KanfitMotorStage(MotorInterface):
 
         @return bool: True if ready False otherwise
         """
-        return True
+        self._mov = moving_p(self._finish_time)
+        return not self._mov
