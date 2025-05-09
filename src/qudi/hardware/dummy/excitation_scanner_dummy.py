5ca9f0feb3b38677307f5b1ca7af40fd9c449fd9
Hugo Levy-Falk <hugo@klafyvel.me>
2025-05-10 00:45:09 +0200

    feat: Add a sampled finite state interface and update the excitation scanner dummy to use it.

--- src/qudi/hardware/dummy/excitation_scanner_dummy.py
+++ src/qudi/hardware/dummy/excitation_scanner_dummy.py
@@ -1,44 +1,51 @@
 import time
 import numpy as np
 from qudi.interface.excitation_scanner_interface import ExcitationScannerInterface, ExcitationScannerConstraints
+from qudi.interface.sampled_finite_state_interface import SampledFiniteStateInterface, transition_to, transition_from, state
+from typing import Iterable, Union, Tuple, Dict, Type, Callable
 
-class ExcitationScannerDummy(ExcitationScannerInterface):
+class ExcitationScannerDummy(ExcitationScannerInterface, SampledFiniteStateInterface):
+    """
+    Copy and paste configuration example:
+    ```
+      excitation_scanner_hardware:
+        module.Class: 'dummy.excitation_scanner_dummy.ExcitationScannerDummy'
+        options:
+          watchdog_delay: 0.2 # default
+    ```
+
+    """
+    _initial_state = "idle"
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._constraints = ExcitationScannerConstraints((0,1),(1,1),(-1e9,1e9),["dummy variable"],[(0,100)],["potatoe"],[int])
         self._exposure_time = 0.001
-        self._scan_start_time = time.perf_counter()
+        self._timer_start = time.perf_counter()
         self._scan_stop_time = time.perf_counter() + self._exposure_time*2000
-        self._repeat_no = 0
+        self._repeat_no = 1
         self._scan_running = False
         self._data_frequency = np.linspace(start=-1e9, stop=1e9, num=2000)
         self._data_count = 1000 * 1/(1 + (self._data_frequency/100e6)**2)
-        self._data_repeat = np.repeat(0, 2000)
+        self._data = np.array([[], [], [], []])
         self._dummy_variable = 0
+        self._current_step = 0
+        self._n_values = 0
     def on_activate(self):
-        pass
+        self.enable_watchdog()
+        self.start_watchdog()
     def on_deactivate(self):
-        pass
+        self.disable_watchdog()
     @property
     def scan_running(self):
-        time_one_spectrum = self._exposure_time*len(self._data_frequency)
-        delay_expired = (time.perf_counter() - self._scan_start_time) > time_one_spectrum
-        if delay_expired:
-            self._scan_stop_time = time.perf_counter()
-            self._scan_running = False
         return self._scan_running
     @property 
     def state_display(self):
-        if self.scan_running:
-            return "Scanning"
-        else:
-            return "Idle"
+        return self.watchdog_state
     def start_scan(self):
-        self._scan_start_time = time.perf_counter()
         self._scan_running = True
     def stop_scan(self):
         self._scan_running = False
-        self._scan_stop_time = time.perf_counter()
     @property 
     def constraints(self):
         return self._constraints
@@ -51,18 +58,13 @@ class ExcitationScannerDummy(ExcitationScannerInterface):
         return self._dummy_variable
     def get_current_data(self) -> np.ndarray:
         "Return current scan data."
-        if self.scan_running:
-            dt = time.perf_counter() - self._scan_start_time
-        else:
-            dt =self._scan_stop_time - self._scan_start_time
-        n_values = min(round(dt/self._exposure_time), len(self._data_frequency))
-        return np.array([self._data_frequency[:n_values], self._data_count[:n_values], self._data_repeat[:n_values]]).T
+        return self._data[:self._n_values, :]
     def set_exposure_time(self, time:float) -> None:
         "Set exposure time for one data point."
         self._exposure_time = time
     def set_repeat_number(self, n:int) -> None:
         "Set number of repetition of each segment of the scan."
-        pass
+        self._repeat_no = n
     def set_idle_value(self, n:float) -> None:
         "Set idle value."
         pass
@@ -75,4 +77,82 @@ class ExcitationScannerDummy(ExcitationScannerInterface):
     def get_idle_value(self) -> float:
         "Get idle value."
         return 0.0
+    @property
+    def data_column_names(self) -> Iterable[str]:
+        return ["Frequency", "Step number", "Time", "Count"]
+    @property
+    def data_column_unit(self) -> Iterable[str]:
+        return ["Hz", "", "s", "c"]
+    @property
+    def data_column_number(self) -> Iterable[int]:
+        return [3]
+    @property
+    def frequency_column_number(self) -> int:
+        return 0
+    @property
+    def step_number_column_number(self) -> int:
+        return 1
+    @property
+    def time_column_number(self) -> int:
+        return 2
+
+    # SampledFiniteStateInterface
+    @state
+    @transition_to(("start_idle", "idle"))
+    @transition_from(("interrupt_scan", ["prepare_scan", "prepare_step", "wait_ready", "record_scan_step"]))
+    def prepare_idle(self):
+        self.log.info("Preparing idle.")
+        self._scan_running = False
+        self.watchdog_event("start_idle")
+    @state
+    @transition_to(("start_scan", "prepare_scan"))
+    def idle(self):
+        if self._scan_running:
+            self.watchdog_event("start_scan")
+    @state
+    @transition_to(("start_prepare_step", "prepare_step"))
+    def prepare_scan(self):
+        self.log.info("Preparing scan.")
+        self._current_step = 0
+        self._scan_start_time = time.perf_counter()
+        self._n_values = 0
+        # Obviously, in a real setting you'd acquire data in record_scan_step, and
+        # not make them up here ;)
+        datapoint_per_scan = len(self._data_frequency)
+        self._data = np.zeros((datapoint_per_scan*self._repeat_no, 4))
+        self._data[:, self.frequency_column_number] = np.tile(self._data_frequency, self._repeat_no)
+        self._data[:, self.step_number_column_number] = np.repeat(range(self._repeat_no), datapoint_per_scan)
+        self._data[:, self.time_column_number] = np.linspace(start=0, stop=self._repeat_no*self._exposure_time*datapoint_per_scan, num=self._repeat_no*datapoint_per_scan)
+        self._data[:, 3] = np.tile(self._data_count, self._repeat_no) + np.random.randn(self._repeat_no*datapoint_per_scan)*50
+        self.watchdog_event("start_prepare_step")
+    @state
+    @transition_to(("start_wait_ready", "wait_ready"))
+    @transition_to(("scan_done", "prepare_idle"))
+    def prepare_step(self):
+        self.log.info(f"Preparing step {self._current_step + 1}/{self._repeat_no}.")
+        self._current_step += 1
+        if self._current_step <= self._repeat_no:
+            self._timer_start = time.perf_counter()
+            self.watchdog_event("start_wait_ready")
+        else:
+            self.log.info("Scan done!")
+            self.watchdog_event("scan_done")
+    @state
+    @transition_to(("start_scan_step", "record_scan_step"))
+    def wait_ready(self):
+        if time.perf_counter() - self._timer_start > 0.5:
+            self.log.info("Sucessfully wasted 0.5s of your time waiting to be ready!")
+            self._timer_start = time.perf_counter()
+            self.watchdog_event("start_scan_step")
+    @state
+    @transition_to(("step_done", "prepare_step"))
+    def record_scan_step(self):
+        time_elapsed = time.perf_counter() - self._timer_start
+        datapoint_per_scan = len(self._data_frequency)
+        time_one_spectrum = self._exposure_time*datapoint_per_scan
+        n = int(time_elapsed / time_one_spectrum + (self._current_step-1)*datapoint_per_scan)
+        self._n_values = min(self._current_step * datapoint_per_scan, self._n_values + n)
+        if self._n_values >= datapoint_per_scan:
+            self.log.info("Step done!")
+            self.watchdog_event("step_done")
 
---
+++ src/qudi/interface/sampled_finite_state_interface.py
@@ -0,0 +1,207 @@
+from typing import Iterable, Union, Tuple, Callable
+import time
+from PySide2 import QtCore
+from qudi.util.mutex import Mutex
+from fysom import Fysom, FysomError
+
+from qudi.core.module import Base
+from qudi.core.configoption import ConfigOption
+
+def state(method: Callable) -> Callable:
+   """Decorator for marking a method as a watchog state.
+
+   The method should not expect any parameter other than `self`.
+
+   Example:
+   ```
+   class MyClass(SampledFiniteStateInterface):
+       @state
+       def say_hi(self):
+           print("hi")
+   ```
+   """
+   method._fysom_state = True
+   return method
+
+def transition_to(*args: Tuple[str, str]) -> Callable:
+   """Decorator for listing the allowed transitions from a given watchdog state (the decorated method).
+
+   Example:
+   ```
+   class MyClass(SampledFiniteStateInterface):
+       @state
+       @transition_to(("hi", "say_hi), ("bar", "say_bar"))
+       def say_foo(self):
+           print("foo")
+       @state
+       @transition_to(("foo", "say_foo"), ("hi", "say_hi"))
+       def say_bar(self):
+           print("bar")
+       @state
+       @transition_to(("foo", "say_foo"), ("bar", "say_bar"))
+       def say_hi(self):
+           print("hi")
+   ```
+   """
+   def decorator(func: Callable) -> Callable:
+       rules = getattr(func, "_fysom_transition_to", [])
+       rules.extend(args)
+       func._fysom_transition_to = rules
+       return func
+   return decorator
+
+def transition_from(*args: Tuple[str, Union[str, Iterable[str]]]) -> Callable:
+   """Decorator for listing the allowed transitions to a given watchdog state (the decorated method).
+
+   # Example:
+   ```
+   class MyClass(SampledFiniteStateInterface):
+       @state
+       @transition_to(("hi", "say_hi), ("bar", "say_bar"))
+       def say_foo(self):
+           print("foo")
+       @state
+       @transition_to(("foo", "say_foo"), ("hi", "say_hi"))
+       def say_bar(self):
+           print("bar")
+       @state
+       @transition_to(("foo", "say_foo"), ("bar", "say_bar"))
+       def say_hi(self):
+           print("hi")
+       @state
+       @transition_from(("sink", "*"))
+       def sink_event(self):
+           print("sank.")
+   ```
+   """
+   def decorator(func: Callable) -> Callable:
+       rules = getattr(func, "_fysom_transition_from", [])
+       rules.extend(args)
+       func._fysom_transition_from = rules
+       return func
+   return decorator
+
+class SampledFiniteStateInterface(Base):
+    """
+    An interface to simplify using a fysom finite state machine to control a process.
+
+    You can mark states and the associated transitions using the `@state`, `@transition_from`, and `@transition_to` decorators.
+
+    An example usage is given in hardware/dummy/excitation_scanner_dummy.py.
+
+    This will define a watchdog that will be polled each `self._watchdog_delay` seconds (this is a `ConfigOption`) and call the appropriate method for the current state. You can transition between state using the `watchdog_event` method.
+
+    Remember to activate and start the watchdog in your `on_activate` method, and
+    to deactivate it in the `on_deactivate` method.
+
+    """
+    _watchdog_delay = ConfigOption(name="watchdog_delay", default=0.2)
+    sig_on_event = QtCore.Signal(str)
+    sig_on_state = QtCore.Signal(str)
+    sig_start_watchdog = QtCore.Signal()
+    sig_stop_watchdog = QtCore.Signal()
+
+    def __init_subclass__(cls, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        states = []
+        events_structure = []
+        events = set()
+        for name, val in cls.__dict__.items():
+            transitions_from = getattr(val, "_fysom_transition_from", [])
+            transitions_to = getattr(val, "_fysom_transition_to", [])
+            if getattr(val, "_fysom_state", False):
+                states.append(name)
+            for (event, from_state) in transitions_from:
+                events_structure.append(dict(name=event, src=from_state, dst=name))
+                events.add(event)
+            for (event, to_state) in transitions_to:
+                events_structure.append(dict(name=event, src=name, dst=to_state))
+                events.add(event)
+        cls.__fysom_events_structure = events_structure
+        cls.__fysom_events = events
+        cls.__fysom_states = states
+        if "_initial_state" not in cls.__dict__.keys():
+            raise NotImplementedError("Subclasses of SampledFiniteStateInterface must have an `_initial_state` string attribute.")
+        for event in events:
+            setattr(cls, "sig_on_event_" + event, QtCore.Signal())
+        for state in states:
+            setattr(cls, "sig_on_state_" + state, QtCore.Signal())
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._watchdog_timer = QtCore.QTimer(parent=self)
+        self._watchdog_lock = Mutex()
+        self._watchdog_running = False
+        callbacks_structure = {}
+        for event in self.__fysom_events:
+            specific_callback = getattr(self, "sig_on_event_" + event).emit
+            callbacks_structure["on_" + event] = lambda _: (specific_callback(), self.sig_on_event.emit(event))
+        for state in self.__fysom_states:
+            specific_callback = getattr(self, "sig_on_state_" + state).emit
+            callbacks_structure["on_" + state] = lambda _: (specific_callback(), self.sig_on_state.emit(state))
+        self._watchdog_state = Fysom({
+            'initial': self._initial_state,
+            'events': self.__fysom_events_structure,
+            'callbacks': callbacks_structure,
+        })
+
+    def __watchdog(self):
+        time_start = time.perf_counter()
+        watchdog_state = self.watchdog_state
+        try:
+            callback = getattr(self, watchdog_state)
+            callback()
+        except FysomError:
+            self.log.error("There is an error in your Fysom state machine! stopping the watchdog now.")
+            self._watchdog_running = False
+            self.log.exception("")
+        except:
+            self.log.exception("")
+        finally:
+            if self._watchdog_running:
+                time_end = time.perf_counter()
+                time_overhead = time_end-time_start
+                new_time = max(0, self._watchdog_delay - time_overhead)
+                self._watchdog_timer.start(new_time*1000)
+
+    @property
+    def watchdog_state(self) -> str:
+        """Query the current state of the watchdog."""
+        with self._watchdog_lock:
+            return self._watchdog_state.current
+
+    def watchdog_event(self, event) -> None:
+        "Trigger a watchdog event that must have beed declared beforehand."
+        with self._watchdog_lock:
+            self._watchdog_state.trigger(event)
+
+    def enable_watchdog(self) -> None:
+        "Enable the watchdog mechanism."
+        self._watchdog_timer.setSingleShot(True)
+        self._watchdog_timer.timeout.connect(self.__watchdog, QtCore.Qt.QueuedConnection)
+        self.sig_start_watchdog.connect(self.__start_watchdog, QtCore.Qt.QueuedConnection)
+        self.sig_stop_watchdog.connect(self.__stop_watchdog, QtCore.Qt.QueuedConnection)
+
+    def disable_watchdog(self) -> None:
+        "Disable the watchdog mechanism. Will interrupt the watchdog if necessary."
+        if self._watchdog_running:
+            self.stop_watchdog()
+        self._watchdog_timer.timeout.disconnect()
+        self.sig_start_watchdog.disconnect(self.__start_watchdog)
+        self.sig_stop_watchdog.disconnect(self.__stop_watchdog)
+
+    def __start_watchdog(self) -> None:
+        self._watchdog_running = True
+        self._watchdog_timer.start(self._watchdog_delay * 1000)
+
+    def __stop_watchdog(self) -> None:
+        self._watchdog_running = False
+
+    def start_watchdog(self) -> None:
+        "Start the watchdog. The mechanism must have been enabled beforehand."
+        self.sig_start_watchdog.emit()
+
+    def stop_watchdog(self) -> None:
+        "Interrupt the watchdog."
+        self.sig_stop_watchdog.emit()
+
