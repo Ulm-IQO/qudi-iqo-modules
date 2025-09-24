7b8ac59595288d70dbdc49cff3a9d42ce14ab9b7
Tobias Spohn <kai.spohn@uni-ulm.de>
2025-09-24 21:15:25 +0200

    laser logic

--- src/qudi/logic/laser_logic.py
+++ src/qudi/logic/laser_logic.py
@@ -28,7 +28,7 @@ from qudi.util.mutex import RecursiveMutex
 from qudi.core.connector import Connector
 from qudi.core.configoption import ConfigOption
 from qudi.core.module import LogicBase
-from qudi.interface.simple_laser_interface import ControlMode, ShutterState, LaserState
+from qudi.interface.simple_laser_interface import ControlMode, ShutterState, LaserState, SimpleLaserInterface
 
 
 class LaserLogic(LogicBase):
@@ -42,7 +42,7 @@ class LaserLogic(LogicBase):
             laser: laser_hardware
     """
 
-    _laser = Connector(name='laser', interface='SimpleLaserInterface')
+    _laser = Connector(name='laser', interface=SimpleLaserInterface)
 
     # waiting time between queries im seconds
     _query_interval = ConfigOption(name='query_interval', default=0.2)
