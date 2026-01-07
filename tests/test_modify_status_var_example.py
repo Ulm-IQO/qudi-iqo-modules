7b949635254b76e08d4eae254ff316bb79515e9c
Mohit <mohit.shah@uni-ulm.de>
2026-01-07 12:08:16 +0100

    added tests for scanning modules

--- tests/test_modify_status_var_example.py
+++ tests/test_modify_status_var_example.py
@@ -20,6 +20,7 @@ You should have received a copy of the GNU Lesser General Public License along w
 If not, see <https://www.gnu.org/licenses/>.
 """
 
+import time
 import pytest
 from qudi.util.yaml import yaml_load, yaml_dump
 from qudi.util.paths import get_module_app_data_path
@@ -119,32 +120,24 @@ def dump_status_variables(vars, file_path):
         print("Failed to save status variables:", e)
 
 
-def test_status_vars(qudi_instance, qt_app):
+def test_status_vars(logic_instance):
     """ 
     Modifying a specific saved status variable for a specific module
 
     Parameters
     ----------
-    qudi_instance : fixture
-        Running qudi instance
-    qt_app : fixture
-        qt app instance
+    logic_instance : fixture
+        logic instance
     """    
-    module_manager = qudi_instance.module_manager
-    qudi_instance._configure_qudi()
-    try:
-        module_manager.modules[LOGIC_MODULE]._load()
-    except Exception as e:
-        print(f'cant load {LOGIC_MODULE} , {e}')
 
-    logic_instance = module_manager.modules[LOGIC_MODULE].instance
+
     status_var_file_path = get_status_var_file(logic_instance)
     status_vars = load_status_var(status_var_file_path)
-    #print(f'Status variables are {status_vars}')
     modified_vars = modify_status_var(status_vars, STATUS_VAR, VALUE)
     dump_status_variables(modified_vars, status_var_file_path)
+    time.sleep(5)
 
-def test_status_vars_changed(logic_instance, qudi_instance):
+def test_status_vars_changed(logic_instance):
     """
     Test whether the status variable has changed
     
@@ -152,8 +145,6 @@ def test_status_vars_changed(logic_instance, qudi_instance):
     ----------
     logic_instance : fixture
         Remote logic instance
-    qudi_instance : fixture
-        So that Qudi objects don't go out of scope
     """    
     status_variable = '_' + STATUS_VAR
     assert getattr(logic_instance, status_variable) == VALUE
