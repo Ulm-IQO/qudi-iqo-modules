9dc228fe8f157000e35d587b8fae2c46bdb2b23f
Tobias Spohn <kai.spohn@uni-ulm.de>
2026-06-30 19:31:37 +0200

    fixing repr

--- src/qudi/logic/pulsed/sampling_functions.py
+++ src/qudi/logic/pulsed/sampling_functions.py
@@ -101,7 +101,7 @@ class DDMethods(Enum):
         return np.array(self._phases)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.value})"
+        return f"{self.__class__.__module__}.{self.__class__.__name__}({self.value})"
 
 
 class SamplingBase:
@@ -268,7 +268,7 @@ class PulseEnvelope:
         return defaults[self.type.value]
 
     def __repr__(self):
-        return f"{self.__class__.__module__}.{self.__class__.__name__}.{self.value}"
+        return f"{self.__class__.__module__}.{self.__class__.__name__}({repr(self.type)}, {self.parameters})"
 
     @classmethod
     def from_dict(cls, parameters: dict) -> "PulseEnvelope":
